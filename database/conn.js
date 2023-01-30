29f4111ef8229200b77f156d72d8949fbaf28749
KONG LY <paly.kong@z1fsc.com>
2023-01-30 15:36:57 +0700

    CRUD Application with Redux & React Query - Next.js [MongoDB Connection]

---
+++ database/conn.js
@@ -0,0 +1,15 @@
+const MONGO_URI = "mongodb+srv://admin:admin123@nextjscrud.dsyu8wa.mongodb.net/?retryWrites=true&w=majority"
+import mongoose from 'mongoose';
+
+const connectMongo = async () => {
+    try{
+        const {connection} = await mongoose.connect(MONGO_URI);
+        if(connection.readyState == 1){
+            console.log("Database Connected");
+        }
+    }catch (errors){
+        return Promise.reject(errors);
+    }
+}
+
+export default connectMongo;
