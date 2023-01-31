8e9a9cdfff65c529ef8b17656c2cb6d480c4b7b8
KONG LY <paly.kong@z1fsc.com>
2023-01-31 10:24:23 +0700

    CRUD Application with Redux & React Query - Next.js [Schema & Controllers]

--- database/conn.js
+++ database/conn.js
@@ -1,9 +1,8 @@
-const MONGO_URI = "mongodb+srv://admin:admin123@nextjscrud.dsyu8wa.mongodb.net/?retryWrites=true&w=majority"
 import mongoose from 'mongoose';
 
 const connectMongo = async () => {
     try{
-        const {connection} = await mongoose.connect(MONGO_URI);
+        const {connection} = await mongoose.connect(process.env.MONGO_URI);
         if(connection.readyState == 1){
             console.log("Database Connected");
         }
