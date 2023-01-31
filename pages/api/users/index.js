0776c9776d92f8433eb0f1bfd382c0a86cc4fc5f
KONG LY <paly.kong@z1fsc.com>
2023-01-31 15:38:46 +0700

    CRUD Application with Redux & React Query - Next.js [Delete Request]

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,5 +1,5 @@
 import connectMongo from '../../../database/conn'
-import { getUsers, postUser, putUser } from '../../../database/controller';
+import { getUsers, postUser, putUser, deleteUser } from '../../../database/controller';
 
 export default async function handler (req, res){
     connectMongo().catch(() => res.status(405).json({error: "Error in the Connection"}));
@@ -18,7 +18,7 @@ export default async function handler (req, res){
             putUser(req, res)
             break;
         case 'DELETE':
-            res.status(200).json({method, name: 'DELETE Request'});
+            deleteUser(req, res)
             break;
         default:
             res.setHeader('Allow', ['GET', "POST", 'PUT', 'DELETE'])
