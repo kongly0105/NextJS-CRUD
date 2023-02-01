6c4df2f4ad2e74f8e3db1e4c3a6be23b584b1ab0
KONG LY <paly.kong@z1fsc.com>
2023-02-01 10:00:42 +0700

    CRUD Application with Redux & React Query - Next.js [React Query]

--- components/table.js
+++ components/table.js
@@ -1,43 +1,46 @@
 /* eslint-disable @next/next/no-img-element */
 import { BiEdit, BiTrashAlt } from "react-icons/bi"
-import data from '../database/data.json';
 import { getUser } from "../lib/helper";
+import { useQuery } from "react-query";
 
 export default function Table() {
 
-    getUser().then(res => console.log(res))
+    const { isLoading, isError, data, error } = useQuery('users', getUser)
 
-  return (
-    <table className="min-w-full table-auto">
-        <thead>
-            <tr className="bg-gray-800">
-                <th className="px-16 py-2">
-                    <span className="text-gray-200">Name</span>
+    if(isLoading) return <div>Employee is Loading...</div>
+    if(isError) return <div>Got Error {error}</div>
+
+    return (
+        <table className="min-w-full table-auto">
+            <thead>
+                <tr className="bg-gray-800">
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200">Name</span>
+                    </th>
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200">Email</span>
+                    </th>
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200">Salary</span>
+                    </th>
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200">Birthday</span>
+                    </th>
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200">Status</span>
+                    </th>
+                    <th className="px-16 py-2">
+                    <span className="text-gray-200">Action</span>
                 </th>
-                <th className="px-16 py-2">
-                    <span className="text-gray-200">Email</span>
-                </th>
-                <th className="px-16 py-2">
-                    <span className="text-gray-200">Salary</span>
-                </th>
-                <th className="px-16 py-2">
-                    <span className="text-gray-200">Birthday</span>
-                </th>
-                <th className="px-16 py-2">
-                    <span className="text-gray-200">Status</span>
-                </th>
-                <th className="px-16 py-2">
-                <span className="text-gray-200">Action</span>
-            </th>
-            </tr>
-        </thead>
-        <tbody className="bg-gray-200">
-           {
-                data.map((obj, i) => <Tr {...obj} key={i} />)
-           }
-        </tbody>
-    </table>
-  )
+                </tr>
+            </thead>
+            <tbody className="bg-gray-200">
+            {
+                    data.map((obj, i) => <Tr {...obj} key={i} />)
+            }
+            </tbody>
+        </table>
+    )
 }
 
 function Tr({id, name, avatar, email, salary, date, status}){
@@ -57,7 +60,7 @@ function Tr({id, name, avatar, email, salary, date, status}){
             <span>{date || "Unknown"}</span>
             </td>
             <td className="px-16 py-2">
-                <button className="cursor"><span className="bg-green-500 text-white px-5 py-1 rounded-full">{status || "Unknown"}</span></button>
+                <button className="cursor"><span className={`${status == "Active" ? 'bg-green-500' : 'bg-rose-500'} text-white px-5 py-1 rounded-full`}>{status || "Unknown"}</span></button>
             </td>
             <td className="flex justify-around gap-5 px-16 py-2">
                 <button className="cursor"><BiEdit size={25} color={"rgb(34,197,94)"} /></button>
